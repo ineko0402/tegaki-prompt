2f8bf51aec6a21f363a7d846ce97c31c49c3b244
ineko0402 <52097123+ineko0402@users.noreply.github.com>
2026-09-17 19:35:08 +0900

    Redesign UI around visual style samples

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'tegaki-prompt-v17';
+const CACHE_NAME = 'tegaki-prompt-v18';
 const ROOT_URL = new URL('./', self.location.href);
 const APP_SHELL = [
   './',
   './index.html',
   './prompt-config.js',
   './manifest.webmanifest',
+  './images/style-samples-v1.webp',
   './icons/icon-192.png',
   './icons/icon-512.png'
 ].map(path => new URL(path, ROOT_URL).href);
